cf06b179e7dae41fda2fc3c72acdb30d09854549
CJ <cjtakawalk@yahoo.com>
2020-08-23 23:31:35 -0700

    First. Selectors and test tests

---
+++ Tests/mainBar.js
@@ -0,0 +1,16 @@
+module.exports = {
+'Main Menu Bar': browser =>{
+    pcpp
+    .click('@builder')
+    .pause(3000)
+    pcpp.navigate()
+    .click('@guides')
+    .pause(3000)
+    pcpp.navigate()
+    .click('@builds')
+    .pause(3000)
+    pcpp.navigate()
+    .click('@products')
+    .pause(3000)
+}
+}
---
+++ assets/pcppObj.js
@@ -0,0 +1,69 @@
+module.exports = {
+    url: 'https://pcpartpicker.com/',
+    elements: {
+        //Home Page
+        builder: {
+            selector: '//*/a[contains(text(), "System Builder")]',
+            locateStrategy: 'xpath'
+        },
+        guides: {
+            selector: '//*/section/div/ul/li/a[@href="/guide/"]',
+            locateStrategy: 'xpath'
+         },
+         builds: {
+             selector: '//*/section/div/ul/li/a[@href="/builds/"]',
+             locateStrategy: 'xpath'
+         },
+         products: {
+             selector: '//*/section/div/ul/li/a[contains(text(), "Browse Products")]',
+             locateStrategy: 'xpath'
+         },
+         //Builder
+         CPU: {
+             selector: '(//*/a[@href="/products/cpu/"])[2]',
+             locateStrategy: 'xpath'
+         },
+         cooler: {
+             selector: '(//*/a[@href="/products/cpu-cooler/"])[2]',
+             locateStrategy: 'xpath'
+         },
+         motherboard: {
+             selector: '(//a[@href="/products/motherboard/"])[2]',
+             locateStrategy: 'xpath'
+         },
+         memory: {
+             selector: '(//a[@href="/products/memory/"])[2]',
+             locateStrategy: 'xpath'
+         },
+         storage: {
+                selector: '(//a[@href="/products/internal-hard-drive/"])[2]',
+                locateStrategy: 'xpath'
+         },
+         videoCard: {
+            selector: '(//a[@href="/products/video-card/"])[2]',
+            locateStrategy: 'xpath'
+        },
+        case: {
+            selector: '(//a[@href="/products/case/"])[2]',
+            locateStrategy: 'xpath'
+        },
+        PSU: {
+            selector: '(//a[@href="/products/power-supply/"])[2]',
+            locateStrategy: 'xpath'
+        },
+
+        //  Add components
+         priceSort: {
+             selector: '//div/section/div/div/div/div/table/thead/tr/th[last()]',
+             locateStrategy: 'xpath'
+         },
+         addTopItem: {
+             selector:'(//button[@data-product-location=""])',
+             locateStrategy: 'xpath'
+         },
+         price: {
+             selector: '(//td[@class="td__price"])[0]',
+             locateStrategy: 'xpath'
+         }
+    }
+}
